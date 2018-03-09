9b68b1b2567d90735c992b6cf8e0584e4a21805e
johmy592 <johmy592@student.liu.se>
2018-03-09 21:12:02 +0100

    Can now convert continuous data to discrete data as a preprocess step

--- apriori.py
+++ apriori.py
@@ -31,17 +31,22 @@ def _get_intervals(data, attr_info, num_bins, ignore_attr=None):
     intervals = []
     types = [attr_info[i][1] for i in range(len(attr_info))]
     for i in range(len(attr_info)):
+ 
         if types[i] not in get_numeric_types() or i==ignore_attr or i-len(attr_info)==ignore_attr:
             intervals.append("N/A")
             continue
-        interval_size = sum([data[j][i] for j in range(len(data))])/(len(data)*num_bins)
-        cur_interval_points = [(i+1)*interval_size for i in range(num_bins-1)]
+
+        data_points = [data[j][i] for j in range(len(data))]
+        minval = min(data_points)
+        diff = max(data_points) - minval
+        interval_size = diff/num_bins
+        cur_interval_points = [minval+(i+1)*interval_size for i in range(num_bins-1)]
         cur_intervals = []
         for i in range(len(cur_interval_points)):
             if i == 0:
                 cur_intervals.append((-float('Inf'),cur_interval_points[i]))
             else:
-                cur_intervals.appen(cur_interval_points[i-1], cur_interval_points[i])
+                cur_intervals.append((cur_interval_points[i-1], cur_interval_points[i]))
         cur_intervals.append((cur_interval_points[i],float('Inf')))
         intervals.append(cur_intervals)
     return intervals
@@ -49,8 +54,30 @@ def _get_intervals(data, attr_info, num_bins, ignore_attr=None):
 def make_discrete(data, attr_info, num_bins, ignore_attr=None):
     intervals = _get_intervals(data, attr_info, num_bins, ignore_attr)
     print(intervals)
-
+    names = [info[0] for info in attr_info]
+    print(names)
+    name_val_dict = {}
+    for i,name in enumerate(names):
+        name_val_dict[name] = []
+        if not isinstance(intervals[i],list):
+            continue
+ 
+        for j in range(len(intervals[i])):
+            name_val_dict[name].append(intervals[i][j])
+    print(name_val_dict)
+    
+    discrete_data = []
+    for transaction in data:
+        discrete_transaction = []
+        for i,item in enumerate(transaction):
+            name = names[i]
+            for j,interval in enumerate(name_val_dict[name]):
+                if item > interval[0] and item < interval[1]:
+                    discrete_transaction.append(j)
+        discrete_data.append(discrete_transaction)
+    print(discrete_data)
+    return discrete_data, name_val_dict
+                
 def main():
     transactions, attr_info = read_transaction_data("iris.txt")
-    print(transactions)
-    make_discrete(transactions, attr_info, 2, -1)
+    discrete_transactions, translation_dict = make_discrete(transactions, attr_info, 3, -1)
